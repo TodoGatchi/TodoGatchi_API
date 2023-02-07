dd80e5ef7ad0333de418632a3d85feb440d5bbed
Josh <josh@sf-iron.com>
2023-02-06 22:11:58 -0800

    added corsOptions and morgan; setup dev and prod build scripts

--- index.ts
+++ index.ts
@@ -1,7 +1,19 @@
 import express, { Express, Request, Response } from "express";
-const port = 8000;
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import cors from 'cors';
 
+const port = 8000;
 const app: Express = express();
+
+const corsOptions = {
+  origin: 'http://localhost:3000', // client port
+  credentials: true,
+}
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors(corsOptions));
 
 app.get("/", (req: Request, res: Response) => {
   res.send('hello from express')
