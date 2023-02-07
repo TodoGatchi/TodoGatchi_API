a2f6d23e169e3c202e2407f1140781d0dca3c567
Josh <josh@sf-iron.com>
2023-02-06 23:33:37 -0800

    Added models folder and connected to controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,5 +1,11 @@
 import { Request, Response } from 'express';
+import * as model from '../models';
 
-export const test = (re: Request, res: Response) => {
-  res.send("hello from controllers")
+export const test = (req: Request, res: Response) => {
+  const testData = {
+    username: "theGang",
+    pets: ["Elliott", "Momo"]
+  }
+  const result = model.test(testData)
+  res.send(result)
 }
